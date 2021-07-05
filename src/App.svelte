b2e9f05e8e08e873bd6d760d3c1a4fde5c37bb04
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-05 22:04:41 +0700

    feat: Add og:url

--- src/App.svelte
+++ src/App.svelte
@@ -147,6 +147,7 @@
   />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+  <meta property="og:url" content={fullVersionLink} />
   <meta property="og:type" content="website" />
   <meta property="og:title" content="Resumette" />
   <meta
