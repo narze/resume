3ff9efc5970eb16bb963181a0a3b966ba6adc891
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-05 21:47:17 +0700

    feat: Add og & twitter meta

--- src/App.svelte
+++ src/App.svelte
@@ -13,6 +13,7 @@
     sourceLink,
     technologies,
     workExperiences,
+    ogImageUrl,
   } from "./data"
 
   let editMode = false
@@ -135,6 +136,29 @@
     or <a href={sourceLink} target="_blank" rel="noopener">source</a>)
   </footer>
 </main>
+
+<svelte:head>
+  <title>Resumette</title>
+
+  <meta name="title" content="Resumette" />
+  <meta
+    name="description"
+    content="Interactive & printable résumé with tailored skills fitted in single letter page."
+  />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="Resumette" />
+  <meta
+    property="og:description"
+    content="Interactive & printable résumé with tailored skills fitted in single letter page."
+  />
+  <meta property="og:image" content={ogImageUrl} />
+
+  <meta name="twitter:title" content="Resumette" />
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:image" content={ogImageUrl} />
+</svelte:head>
 
 <style>
   main {
--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,7 @@
 export const fullVersionLink = "https://manassarn-resume.vercel.app"
 export const sourceLink = "https://github.com/narze/resume"
+export const ogImageUrl =
+  "https://og-image.vercel.app/Resumette.png?theme=light&md=1&fontSize=200px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg"
 
 export const introData = {
   name: "Manassarn Manoonchai",
