8dc19d7bcb582c828e14a251b3bf75c2b73370cc
Manassarn Manoonchai <manassarn@gmail.com>
2022-02-04 04:58:48 +0000

    Change full version link

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-export const fullVersionLink = "https://manassarn-resume.vercel.app"
+export const fullVersionLink = "https://resume.narze.live"
 export const sourceLink = "https://github.com/narze/resume"
 export const ogImageUrl =
   "https://og-image.vercel.app/Resumette.png?theme=light&md=1&fontSize=200px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg"
